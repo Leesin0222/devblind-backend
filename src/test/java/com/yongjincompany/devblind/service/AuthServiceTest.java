ca56f170959f61874e19193145a0db016ab6792d
leeyongjin <dragon02229@naver.com>
2025-08-13 18:10:47 +0900

    7일차 테스트 코드의 import문 clean up

--- src/test/java/com/yongjincompany/devblind/service/AuthServiceTest.java
+++ src/test/java/com/yongjincompany/devblind/service/AuthServiceTest.java
@@ -26,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.startsWith;
